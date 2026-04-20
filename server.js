3c313e9bd82a41648a661d99e0d95620b6716316
ELIE-MANI <eliemaniraguha@gmail.com>
2026-04-20 18:47:00 +0200

    Initial commit - backend setup

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+app.use(express.json ());
+
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Welcome to the Pharmacy Management System API'
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
+
