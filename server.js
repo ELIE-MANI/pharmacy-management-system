c6f74d0a3b1621b358df103567f562596ab97a17
ELIE-MANI <eliemaniraguha@gmail.com>
2026-04-27 20:16:32 +0200

    changed sync to authenticate

--- server.js
+++ server.js
@@ -31,15 +31,15 @@ app.use('/suppliers', supplierRoutes);
 app.use('/medicines', medicineRoutes);
 app.use('/customers', customerRoutes);
 app.use('/prescriptions', prescriptionRoutes);
-sequelize.sync({force: false })
+sequelize.authenticate()
 .then(() => {
-   console.log('Database synced');
+   console.log('Database connected successfully');
    app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`); 
     });
 })
 .catch(err => {
-    console.error('Unable to sync database:', err);
+    console.error('Unable to connect to the database:', err);
 });
 
 
